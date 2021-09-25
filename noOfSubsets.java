b6d04282e07ca222cb40f3bf4b52872f550b3b7a
Nivethitha21 <59059126+Nivethitha21@users.noreply.github.com>
2021-09-25 23:47:51 +0530

    No of subsets in given set whose sum = target

---
+++ noOfSubsets.java
@@ -0,0 +1,21 @@
+
+	     int dp[][] = new int[n+1][sum+1];
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        for(int j = 1; j <= sum; j++)
+            dp[0][j] = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= sum; j++)
+            {
+                if(nums[i-1] > j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+            }
+        }
+  
+            return dp[n][sum];
+}}
